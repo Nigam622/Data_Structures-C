1daf6e047a7e6195ec3ac4d2ad097b8b79c3e434
NIGAM.N <80585462+Nigam622@users.noreply.github.com>
2022-05-18 15:47:22 +0530

    Update sll.c

--- sll.c
+++ sll.c
@@ -1,167 +1,109 @@
-//Program 7 - SLL
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
-struct node
+struct stud
 {
-int sem,phno;
-char name[20],branch[10],usn[10];
-struct node*link ;
-};
-typedef struct node *Node;
-Node getnode()
+    char usn[11],name[15],branch[4],phno[11];
+    int sem;
+    struct stud *next;
+}*f=NULL,*r=NULL,*t=NULL;
+void ins(int ch)
 {
-Node temp;
-int sem,phno;
-char name[20],usn[10],branch[10];
-temp=(Node)malloc(sizeof(struct node));
-if(temp==NULL)
-{
-printf("memory not available");
-exit(0);
+    t=(struct stud*)malloc(sizeof(struct stud));
+    printf("\nEnter USN:");
+    scanf("%s",t->usn);
+    printf("Enter Name:");
+    scanf("%s",t->name);
+    printf("Enter Branch:");
+    scanf("%s",t->branch);
+    printf("Enter Sem:");
+    scanf("%d",&t->sem);
+    printf("Enter Phno:");
+    scanf("%s",t->phno);
+    t->next=NULL;
+    if(!r)
+        f=r=t;
+    else
+    {
+        if(ch)
+        {
+            r->next=t;
+            r=t;
+        }
+        else
+        {
+            t->next=f;
+            f=t;
+        }
+    }
 }
-printf("Enter the student details");
-printf("\nName , USN , Branch, sem, Phone no. :");
-scanf("%s%s%s%d%d",name,usn,branch,&temp->sem,&temp->phno);
-strcpy(temp->name,name);
-strcpy(temp->usn,usn);
-strcpy(temp->branch,branch);
-temp->link=NULL;
-return temp;
-}
-Node insert_front(Node first)
+void del(int ch)
 {
-Node temp;
-temp=getnode();
-temp->link=first;
-return temp;
+    if(!f)
+        printf("\nList Empty");
+    else
+    {
+        struct stud *t1;
+        if(f==r)
+        {
+            t1=f;
+            f=r=NULL;
+        }
+        else if(ch)
+        {
+            t1=r;
+            for(t=f;t->next!=r;t=t->next)
+                r=t;
+            r->next=NULL;
+        }
+        else
+        {
+            t1=f;
+            f=f->next;
+        }
+        printf("\nElement deleted is:\n");
+        printf("USN:%s\nName:%s\nBranch:%s\nSem:%d\nPhno:%s\n",t1->usn,t1->name,t1->branch,t1->sem,t1->phno);
+        free(t1);
+    }
 }
-Node insert_rear(Node first)
+void disp()
 {
-Node cur,prev,temp;
-temp=getnode();
-if(first==NULL)
-{
-return temp;
+    if(!f)
+        printf("\nList Empty!!!");
+    else
+        printf("\nList elements are:\n");
+    for(t=f;t;t=t->next)
+        printf("\nUSN:%s\nName:%s\nBranch:%s\nSem:%d\nPhno:%s\n",t->usn,t->name,t->branch,t->sem,t->phno);
 }
-else
+void main()
 {
-cur=first;
-prev=NULL;
-while(cur!=NULL)
-{
-prev=cur;
-cur=cur->link;
-}
-prev->link=temp;
-return first;
-}
-}
-Node delete_front(Node first)
-{
-if(first==NULL)
-{
-printf("SLL is empty");
-return first;
-}
-else
-{
-Node next;
-printf("deleted info is = %s %s %s %d %d",first->name,first-
->usn,first->branch,first->sem,first->phno);
-next=first->link;
-free(first);
-return next;
-}
-}
-Node delete_rear(Node first)
-{
-Node cur,prev=NULL;
-if(first==NULL)
-{
-printf("SLL is empty");
-return NULL;
-}
-else
-{
-Node cur;
-cur = first;
-while(cur->link!=NULL)
-{
-prev=cur;
-cur=cur->link;
-}
-printf("\ndeleted info is = %s %s %s %d %d",cur->name,cur-
->usn,cur->branch,cur->sem,cur->phno);
-prev->link=NULL;
-free(cur);
-return first;
-}
-}
-void display(Node first)
-{
-Node cur;
-if(first==NULL)
-{
-printf("\nNo elements in SLL");
-}
-else
-{
-cur=first;
-while(cur!=NULL)
-{
-printf("\n%s, %s, %s, %d, %d",cur->name,cur->branch,cur-
->usn,cur->phno,cur->sem);
-cur=cur->link;
-}
-}
-}
-void count(Node first)
-{
-Node cur;
-int count=0;
-cur=first;
-if(first==NULL)
-{
-printf("there are no elements in SLL");
-return;
-}
-else
-{
-while(cur!=NULL)
-{
-count++;
-cur=cur->link;
-}
-printf("The number of nodes in SLL = %d",count);
-}
-}
-int main()
-{
-Node first;
-int ch;
-first=NULL;
-while(1)
-{
-printf("\n\n1.Insert front , 2.Insert rear , 3.Delete front , 
-4.Delete rear , 5.Display , 6.Exit");
-printf("\nEnter the choice : ");
-scanf("%d",&ch);
-switch(ch)
-{
-      case 1 : first = insert_front(first);
-      break;
-      case 2 : first = insert_rear(first);
-      break;
-      case 3 : first = delete_front(first);
-      break;
-      case 4 : first = delete_rear(first);
-      break;
-      case 5 : display(first);
-      break;
-      default : printf("invalid 
-      input"); 
-}
-}
-return 0;
+    int ch,n,i;
+    printf("\n........Menu..........,\n");
+    printf("1.Create\n");
+    printf("2.Display\n");
+    printf("3.Insert at end\n");
+    printf("4.Delete at end\n");
+    printf("5.Insert at beg\n");
+    printf("6.Delete at beg\n");
+    printf("7.Exit\n");
+    while(1)
+    {
+        printf("\nEnter choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: printf("\nEnter no. of nodes:");
+                    scanf("%d",&n);
+                    for(i=0;i<n;i++)
+                        ins(0);
+                    break;
+            case 2:disp();break;
+            case 3:ins(1);break;
+            case 4:del(1);break;
+            case 5:ins(0);break;
+            case 6:del(0);break;
+            case 7:exit(0);
+            default:printf("\nInvalid choice!!!!");
+        }
+    }
 }
